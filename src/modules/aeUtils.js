6652af5ca45f308ef88ab1451122ec7133c91301
AE Creations <aecreations@users.noreply.github.com>
2018-03-30 18:39:58 -0700

    Make the debug constants a public member variable of aeUtils

--- src/modules/aeUtils.js
+++ src/modules/aeUtils.js
@@ -7,11 +7,6 @@ ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 const EXPORTED_SYMBOLS = ["aeUtils"];
 
-// Debugging flags - set to false to suppress extraneous JS console messages
-// and diagnostic audio beeps.
-const DEBUG = true;
-const NOISY_DEBUG = false;
-
 // Host app GUIDs
 const HOSTAPP_FX_GUID = "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}";
 const HOSTAPP_TB_GUID = "{3550f703-e582-4d05-9a08-453d09bdfdc6}";
@@ -25,12 +20,11 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 
 
-var aeUtils = {
-  // TO DO: Consider moving this to aeConstants module.  Note that the
-  // PE shell script will need to be modified to read the JS module file.
-  // Set this constant to true if building this extension for Portable Firefox
-  // or Portable Thunderbird
-  PORTABLE_APP_BUILD: false
+let aeUtils = {
+  // Debugging flags - set to false to suppress extraneous JS console messages
+  // and diagnostic audio beeps.
+  DEBUG: true,
+  NOISY_DEBUG: false,
 };
 
 
@@ -356,7 +350,7 @@ aeUtils.beep = function ()
 
 aeUtils.debugBeep = function ()
 {
-  if (DEBUG && NOISY_DEBUG) {
+  if (this.DEBUG && this.NOISY_DEBUG) {
     this._beep();
   }
 };
@@ -372,7 +366,7 @@ aeUtils._beep = function ()
 
 aeUtils.log = function (aMessage) 
 {
-  if (DEBUG) {
+  if (this.DEBUG) {
     var consoleSvc = Components.classes["@mozilla.org/consoleservice;1"]
                                .getService(Components.interfaces
 					             .nsIConsoleService);
