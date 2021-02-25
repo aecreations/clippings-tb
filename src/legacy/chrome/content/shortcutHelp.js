f2cda82668715cbde8eef8ba41797fbcc4d8da7c
AE Creations <aecreations@users.noreply.github.com>
2021-02-24 20:32:51 -0800

    Re-enabled ability to print or export to HTML the shortcut key list from shortcut list window
    (part of issue #53)

--- src/legacy/chrome/content/shortcutHelp.js
+++ src/legacy/chrome/content/shortcutHelp.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const {OS} = ChromeUtils.import("resource://gre/modules/osfile.jsm");
 const {FileUtils} = ChromeUtils.import("resource://gre/modules/FileUtils.jsm");
 const {aeConstants} = ChromeUtils.import("resource://clippings/modules/aeConstants.js");
 const {aeUtils} = ChromeUtils.import("resource://clippings/modules/aeUtils.js");
@@ -13,7 +14,7 @@ const Ci = Components.interfaces;
 const OUTPUT_PRINTER = 0;
 const OUTPUT_FILE = 1;
 
-var gDlgArgs, gStrBundle, gEditor, gIsShortcutKeyHelpContentGenerated;
+var gDlgArgs, gStrBundle;
 
 
 function $(aID)
@@ -49,13 +50,6 @@ function init()
     treeItem.appendChild(treeRow);
     treeChildren.appendChild(treeItem);
   }
-
-  // Hidden HTML editor - HTML export hack!
-  gEditor = $("html-export");
-  gEditor.src = "export.html";
-  gEditor.makeEditable("html", false);
-
-  gIsShortcutKeyHelpContentGenerated = false;
 
   if (gDlgArgs.showInsertClippingCmd) {
     $("insert-clipping").hidden = false;
@@ -126,19 +120,8 @@ function save()
 
 function outputShortcutList(aOutputMode)
 {
-  // TEMPORARY
-  window.alert("The selected action is not available right now.");
-  return;
-  // END TEMPORARY
-  
-  if (! gIsShortcutKeyHelpContentGenerated) {
-    generateShortcutKeyHelpContent();
-    gIsShortcutKeyHelpContentGenerated = true;
-  }
-
   var css = "";
   var data = "";
-  var body = gEditor.contentDocument.getElementsByTagName("body")[0];
 
   if (aOutputMode == OUTPUT_PRINTER) {
     // Show printing instructions in the HTML output generated from Thunderbird
@@ -165,16 +148,16 @@ function outputShortcutList(aOutputMode)
   else {
     data = "<body>";
   }
-  data += body.innerHTML + "</body>";
+
+  let body = getShortcutKeyHelpContent();
+
+  data += body + "</body>";
 
   var doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">';
   var meta = '<META http-equiv="Content-Type" content="text/html; charset=UTF-8">';
   var header = "<html><head>" + meta + "<title>" + gStrBundle.getString("shortcutHelpTitle") + "</title>" + css + "</head>";
   var footer = "</html>";
   data = doctype + "\n" + header + "\n" + data + "\n" + footer;
-
-  // Leverage method aeIClippingsService.writeFile()
-  var clippingsSvc = aeClippingsService.getService();
 
   if (aOutputMode == OUTPUT_PRINTER) {
     // Write the HTML data to a temp file.
@@ -183,65 +166,23 @@ function outputShortcutList(aOutputMode)
 
     aeUtils.log("Temp file path: " + tempFile.path);
 
-    var tempFileURL = aeUtils.getURLFromFilePath(tempFile.path);
-
-    try {
-      clippingsSvc.writeFile(tempFileURL, data);
-    }
-    catch (e) {
-      if (e.result === undefined) {
-	aeUtils.alertEx(title, gStrBundle.getString("errorSaveFailed"));
-	return;
-      }
-
-      if (e.result == Components.results.NS_ERROR_OUT_OF_MEMORY) {
-	aeUtils.alertEx(title, gStrBundle.getString("errorOutOfMemory"));
-      }
-      else if (e.result == Components.results.NS_ERROR_FILE_ACCESS_DENIED) {
-	let msg = aeString.format("%s: %s",
-			          gStrBundle.getString("errorAccessDenied"),
-			          aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-	aeUtils.alertEx(title, msg);
-      }
-      else if (e.result == Components.results.NS_ERROR_FILE_IS_LOCKED) {
-	let msg = aeString.format("%s: %s",
-			          gStrBundle.getString("errorFileLocked"),
-			          aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-	aeUtils.alertEx(title, msg);
-      }
-      else if (e.result == Components.results.NS_ERROR_FILE_TOO_BIG) {
-	let msg = aeString.format("%s: %s",
-			          gStrBundle.getString("errorFileTooBig"),
-			          aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-	aeUtils.alertEx(title, msg);
-      }
-      else if (e.result == Components.results.NS_ERROR_FILE_READ_ONLY) {
-	let msg = aeString.format("%s: %s",
-			          gStrBundle.getString("errorFileReadOnly"),
-			          aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-	aeUtils.alertEx(title, msg);
-      }
-      else if (e.result == Components.results.NS_ERROR_FILE_DISK_FULL) {
-	let msg = aeString.format("%s: %s",
-			          gStrBundle.getString("errorDiskFull"),
-			          aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-	aeUtils.alertEx(title, msg);
-      }
-      else {
-	aeUtils.alertEx(title, gStrBundle.getString("errorSaveFailed"));
-      }
-      return;
-    }
+    let writeFileOpts = {
+      encoding: "utf-8",
+      tmpPath: `${tempFile.path}.tmp`
+    };
     
-    // Open the temp file and automatically print it.
-    if (gDlgArgs.printToExtBrowser) {
-      // If on Thunderbird, open the generated HTML document in the default
-      // web browser.
-      tempFile.launch();
-    }
-    else {
-      window.open(tempFileURL);
-    }
+    OS.File.writeAtomic(tempFile.path, data, writeFileOpts).then(() => {
+      // Open the temp file and automatically print it.
+      if (gDlgArgs.printToExtBrowser) {
+	// If on Thunderbird, open the generated HTML document in the default
+	// web browser.
+	tempFile.launch();
+      }      
+    }).catch(aErr => {
+      let msg = gStrBundle.getString("errorSaveFailed") + "\n" + aErr;
+      aeUtils.alertEx(title, msg);
+      return;
+    });
   }
   else if (aOutputMode == OUTPUT_FILE) {
     // Save shortcut key list to an HTML document.
@@ -264,54 +205,15 @@ function outputShortcutList(aOutputMode)
           oldFile.remove(false);
         }
 
-	var url = fp.fileURL.QueryInterface(Ci.nsIURI).spec;
-
-	try {
-	  clippingsSvc.writeFile(url, data);
-	}
-	catch (e) {
-	  if (e.result === undefined) {
-	    aeUtils.alertEx(title, gStrBundle.getString("errorSaveFailed"));
-	    return;
-	  }
-	  
-          if (e.result == Components.results.NS_ERROR_OUT_OF_MEMORY) {
-            aeUtils.alertEx(title, gStrBundle.getString("errorOutOfMemory"));
-          }
-          else if (e.result == Components.results.NS_ERROR_FILE_ACCESS_DENIED) {
-            let msg = aeString.format("%s: %s",
-                                      gStrBundle.getString("errorAccessDenied"),
-                                      aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-            aeUtils.alertEx(title, msg);
-          }
-          else if (e.result == Components.results.NS_ERROR_FILE_IS_LOCKED) {
-            let msg = aeString.format("%s: %s",
-                                      gStrBundle.getString("errorFileLocked"),
-                                      aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-            aeUtils.alertEx(title, msg);
-          }
-          else if (e.result == Components.results.NS_ERROR_FILE_TOO_BIG) {
-            let msg = aeString.format("%s: %s",
-                                      gStrBundle.getString("errorFileTooBig"),
-                                      aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-            aeUtils.alertEx(title, msg);
-          }
-          else if (e.result == Components.results.NS_ERROR_FILE_READ_ONLY) {
-            let msg = aeString.format("%s: %s",
-                                      gStrBundle.getString("errorFileReadOnly"),
-                                      aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-            aeUtils.alertEx(title, msg);
-          }
-          else if (e.result == Components.results.NS_ERROR_FILE_DISK_FULL) {
-            let msg = aeString.format("%s: %s",
-                                      gStrBundle.getString("errorDiskFull"),
-                                      aeConstants.SHORTCUT_HELP_PRINT_FILENAME);
-            aeUtils.alertEx(title, msg);
-          }
-	  else {
-            aeUtils.alertEx(title, gStrBundle.getString("errorSaveFailed"));
-          }
-	}
+	let writeFileOpts = {
+	  encoding: "utf-8",
+	  tmpPath: `${fp.file.path}.tmp`
+	};
+	
+	OS.File.writeAtomic(fp.file.path, data, writeFileOpts).catch(aErr => {
+	  let msg = gStrBundle.getString("errorSaveFailed") + "\n" + aErr;
+	  aeUtils.alertEx(title, msg);
+	});
       }
     };
 
@@ -320,62 +222,46 @@ function outputShortcutList(aOutputMode)
 }
 
 
-function generateShortcutKeyHelpContent()
+function getShortcutKeyHelpContent()
 {
-  gEditor.focus();
-  var doc = gEditor.contentDocument;
-  var body = doc.getElementsByTagName("body")[0];
-  var hdg = doc.createElement("h1");
-  var txtHdg = doc.createTextNode(gStrBundle.getString("shortcutHelpTitle"));
-  hdg.appendChild(txtHdg);
-  body.appendChild(hdg);
+  let rv = "";
 
-  var pAppInfo = doc.createElement("p");
-  pAppInfo.className = "app-info";
-  pAppInfo.style.fontSize = "small";
-
-  var txtAppInfo = doc.createTextNode(gStrBundle.getFormattedString("clippingsOnHostAppNameAndVer", [aeUtils.getHostAppName(), aeUtils.getHostAppVersion()]));
-  pAppInfo.appendChild(txtAppInfo);
-  body.appendChild(pAppInfo);
-
-  var pHelp = doc.createElement("p");
-  var helpStr = gStrBundle.getFormattedString("shortcutInstr", [gStrBundle.getString("pasteIntoTb")]);
-
-  var txtHelp = doc.createTextNode(helpStr);
-  pHelp.appendChild(txtHelp);
-  body.appendChild(pHelp);
-
-  var table = doc.createElement("table");
-  table.setAttribute("border", "2");
-  var thead = doc.createElement("thead");
-  var trHdr = doc.createElement("tr");
-  var thShortcutKey = doc.createElement("th");
-  var txtShortcutKey = doc.createTextNode(gStrBundle.getString("shortcutKeyTitle"));
-  thShortcutKey.appendChild(txtShortcutKey);
-  var thClipping = doc.createElement("th");
-  var txtClipping = doc.createTextNode(gStrBundle.getString("clippingNameTitle"));
-  thClipping.appendChild(txtClipping);
-  trHdr.appendChild(thShortcutKey);
-  trHdr.appendChild(thClipping);
-  thead.appendChild(trHdr);
-  table.appendChild(thead);
-  
-  var tbody = doc.createElement("tbody");
-  var keyMap = gDlgArgs.keyMap;
-
-  for (let key in keyMap) {
-    var tr = doc.createElement("tr");
-    var tdKey = doc.createElement("td");
-    var txtKey = doc.createTextNode(key);
-    tdKey.appendChild(txtKey);
-    var tdClipping = doc.createElement("td");
-    var txtClipping = doc.createTextNode(keyMap[key].name);
-    tdClipping.appendChild(txtClipping);
-    tr.appendChild(tdKey);
-    tr.appendChild(tdClipping);
-    tbody.appendChild(tr);
+  if (getShortcutKeyHelpContent.htmlBody) {
+    rv = getShortcutKeyHelpContent.htmlBody;
   }
+  else {
+    let pageHdg = gStrBundle.getString("shortcutHelpTitle");
+    let appInfo = gStrBundle.getFormattedString(
+      "clippingsOnHostAppNameAndVer",
+      [aeUtils.getHostAppName(), aeUtils.getHostAppVersion()]
+    );
+    let helpStr = gStrBundle.getFormattedString("shortcutInstr", [gStrBundle.getString("pasteIntoTb")]);
+    let thShortcut = gStrBundle.getString("shortcutKeyTitle");
+    let thClipping = gStrBundle.getString("clippingNameTitle");
+    
+    let tbody = "";
 
-  table.appendChild(tbody);
-  body.appendChild(table);
+    for (let key of gDlgArgs.keyMap.keys()) {
+      tbody += `<tr><td>${key}</td><td>${gDlgArgs.keyMap.get(key).name}</td></tr>\n`;
+    }
+
+    rv = getShortcutKeyHelpContent.htmlBody = `<body>
+  <h1>${pageHdg}</h1>
+  <p class="app-info" style="font-size:small">${appInfo}</p>
+  <p>${helpStr}</p>
+  <table border="2">
+    <thead>
+      <th>${thShortcut}</th>
+      <th>${thClipping}</th>
+    </thead>
+    <tbody>
+      ${tbody}
+    </tbody>
+  </table>  
+</body>`;
+  }
+  
+  return rv;
 }
+
+getShortcutKeyHelpContent.htmlBody = "";
