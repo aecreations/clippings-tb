53213b088009d3c779e3aff8eb70bf3077650e16
AE Creations <aecreations@users.noreply.github.com>
2024-09-25 00:01:02 -0700

    Bug fix: Context menu in clippings tree shouldn't appear for "No items found"
    (issue #193)
    - Back-ported from version 7.0

--- src/pages/clippingsMgr.js
+++ src/pages/clippingsMgr.js
@@ -4795,6 +4795,12 @@ async function buildClippingsTree()
 
     events: {
       show: function (aOpts) {
+        let treeItemSpan = aOpts.$trigger[0].firstChild;
+        if (treeItemSpan.classList.contains("fancytree-statusnode-nodata")) {
+          // Hide the context menu if "No items found" in the search results
+          // is selected.
+          return false;
+        }
         return (! gIsClippingsTreeEmpty);
       }
     },
